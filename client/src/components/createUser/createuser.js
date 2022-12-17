44d6bb972481b235db1f823caa4ceefc789ca705
m7madnat <m7madnatseh1999@gmail.com>
2022-12-17 18:20:46 +0200

    Update createuser.js

--- client/src/components/createUser/createuser.js
+++ client/src/components/createUser/createuser.js
@@ -1,50 +1,51 @@
-import axios from "axios";
 import React, { useState } from "react";
+import { Api } from "../../api/Api";
+import { Link } from "react-router-dom";
 
 export const CreateUser = () => {
-
-    const [name, setName] = useState('');
-    const [accountIds, setAccountIds] =  useState('');
-
-
-
-
-    const addUser = (e) => {
-        e.preventDefault();
-        const user = { name ,accountIds };    
-        if(accountIds === ""){
-            user.accountIds = [];
-        }
-        else{
-            user.accountIds = accountIds.split(',');
-        }
-        try{
-            axios.post('http://localhost:5001/api/users/add', user);
-        }
-        catch(err){
-            console.log(err);
-        }
+  const [name, setName] = useState("");
+  const [accountIds, setAccountIds] = useState("");
+  const addUser = (e) => {
+    e.preventDefault();
+    const user = { name, accountIds };
+    if (accountIds === "") {
+      user.accountIds = [];
+    } else {
+      user.accountIds = accountIds.split(",");
     }
+    try {
+      Api.post("/users/add", user)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((e) => console.log(e));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    return (
-        <div>
-            <h1>Create User</h1>
-            <form>
-                <label>Name</label>
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                <label>Account Ids</label>
+  return (
+    <div>
+      <h1>Create User</h1>
+      <form>
+        <label>Name</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label>Account Ids</label>
 
-                <input type="text" value={accountIds} onChange={(e) => setAccountIds(e.target.value)} />
-                <button onClick={addUser}>Create</button>
-            </form>
-        </div>
-    )
-}
-
-
-
-
-
-
-
-
+        <input
+          type="text"
+          value={accountIds}
+          onChange={(e) => setAccountIds(e.target.value)}
+        />
+        <button onClick={addUser}>Create</button>
+        <h1>
+          <Link to="/users">Check all users</Link>
+        </h1>
+      </form>
+    </div>
+  );
+};
