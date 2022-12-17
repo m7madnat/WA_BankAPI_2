3c74472c88e9ece2707de3b40b7d4e018379a69b
m7madnat <m7madnatseh1999@gmail.com>
2022-12-17 15:18:19 +0200

    Update Api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,10 +1,25 @@
 import axios from 'axios';
 
-let myUrl = 'http://localhost:5001/api';
+// let myUrl = 'http://localhost:5001/api';
 
-if (process.env.NODE_ENV === 'production') {
-  myUrl = 'api';
+// if (process.env.NODE_ENV === 'production') {
+//   myUrl = 'api';
+// }
+// export const Api = axios.create({
+//   baseURL: myUrl,
+// });
+
+export function Api(options = {}) {
+  const { headers = {} } = options;
+
+  const url =
+    process.env.NODE_ENV === "production" ? "/" : "http://localhost:5001/api";
+
+  return axios.create({
+    baseURL: url,
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+  });
 }
-export const Api = axios.create({
-  baseURL: myUrl,
-});
