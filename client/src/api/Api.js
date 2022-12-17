a62d55f0441d2755cb3af55496cc997dfca0872a
m7madnat <m7madnatseh1999@gmail.com>
2022-12-17 18:09:27 +0200

    Update Api.js

--- client/src/api/Api.js
+++ client/src/api/Api.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-let myUrl = 'http://localhost:5001/api';
+let myUrl = "http://localhost:5001/api";
 
-if (process.env.NODE_ENV === 'production') {
-  myUrl = 'api';
+if (process.env.NODE_ENV === "production") {
+  myUrl = "https://wa-bankapi.onrender.com/api";
 }
 export const Api = axios.create({
   baseURL: myUrl,
